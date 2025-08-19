bd81ca2e780184eb440f5cde14fdd42e690a2402
Nathan Reymer <reymer_n@hotmail.com>
2025-08-18 23:20:43 -0400

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,15 +8,15 @@ import Contact from "@/components/Contact";
 import { client } from '@/sanity/lib/client'
 import { SEO_QUERY } from '@/sanity/lib/queries'
 
-// export async function generateMetadata() {
-//   const seo =await client.fetch(SEO_QUERY)
-//   return {
-//     title: seo.title,
-//     description: seo.HomepageSEO.metaDescription,
-//     keywords: seo.HomepageSEO.metaTags,
-//     icons: "/images/favicon.png",
-//   }
-// }
+export async function generateMetadata() {
+  const seo = await client.fetch(SEO_QUERY)
+  return {
+    title: seo.title,
+    description: seo.HomepageSEO.metaDescription,
+    keywords: seo.HomepageSEO.metaTags,
+    icons: "/images/favicon.png",
+  }
+}
 
 export default function Home() {
   return (
