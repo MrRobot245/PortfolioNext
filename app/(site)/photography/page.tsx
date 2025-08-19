a4beb05caa78bc5c87494f039d75f943af9e3359
WeedMD-Support <reymer_n@outlook.com>
2025-08-18 23:40:05 -0400

    Update

--- app/(site)/photography/page.tsx
+++ app/(site)/photography/page.tsx
@@ -1,23 +1,25 @@
-import ContactHero from "@/components/ContactHero";
+import Hero from "@/components/Hero";
 import { client } from '@/sanity/lib/client'
 import { SEO_QUERY } from '@/sanity/lib/queries'
 
-// export async function generateMetadata() {
-//   const seo = await client.fetch(SEO_QUERY)
-//   return {
-//     title: seo.title + " - Contact",
-//     description: seo.ContactSEO.metaDescription,
-//     keywords: seo.ContactSEO.metaTags,
-//     icons: "/images/favicon.png",
-//   }
-// } ``
+export async function generateMetadata() {
+    const seo = await client.fetch(SEO_QUERY)
+    return {
+        title: seo.title + " - Contact",
+        description: seo.PhotographySEO.metaDescription,
+        keywords: seo.PhotographySEO.metaTags,
+        icons: "/images/favicon.png",
+    }
+} ``
 
 export default function Photography() {
 
 
     return (
         <>
-            {/* <ContactHero /> */}
+            <main>
+                <Hero />
+            </main>
         </>
 
     );
